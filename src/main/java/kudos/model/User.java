7f7582d84ab865b0405d65be60f83ed4ef8aef9c
Your Name <p998bjg>
2015-08-04 15:58:27 +0300

    fixed gradle file

--- src/main/java/kudos/model/User.java
+++ src/main/java/kudos/model/User.java
@@ -1,5 +1,6 @@
 package kudos.model;
 
+
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.index.Indexed;
 import org.springframework.data.mongodb.core.mapping.Document;
