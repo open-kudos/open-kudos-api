2f001c422cfc5264d49738f23ebeb51949fccd98
Mantas <m.damijonaitis@gmail.com>
2015-08-25 10:52:10 +0300

    documented relations controller, made the documentation reachable in one click, edited readme.md file

--- README.md
+++ README.md
@@ -1,8 +1,14 @@
-open-kudos-readme by KUDOS team
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
+"http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 
-If you want to view KUDOS documentation, follow these steps:
-	1) Run builded KUDOS jar
-	2) Paste this link into your browser: http://localhost:8080/jsondoc-ui.html#
-	3) In the top of the page, you will see input field. Paste this link: http://localhost:8080/jsondocs
-	4) Click "Get documentation"
-	5) Enjoy!
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<title></title>
+</head>
+
+<body>
+To watch documentation, click this link:
+<a href="http://79.98.29.151:8080/jsondoc-ui.html?url=http://79.98.29.151:8080/jsondoc">Documentation</a>
+</body>
+
+</html>
--- src/main/java/kudos/web/controllers/RelationController.java
+++ src/main/java/kudos/web/controllers/RelationController.java
@@ -7,6 +7,7 @@ import kudos.model.Relation;
 import kudos.model.User;
 import kudos.web.exceptions.UserException;
 import org.apache.log4j.Logger;
+import org.jsondoc.core.annotation.*;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -18,16 +19,26 @@ import java.util.Optional;
 /**
  * Created by chc on 15.8.20.
  */
+@Api(description = "service to manage user relations", name = "RelationController")
 @Controller
 @RequestMapping("/relations")
 public class RelationController extends BaseController {
 
     private Logger LOG = Logger.getLogger(RelationController.class);
 
+    @ApiMethod(description = "service to add relation")
+    @ApiParams(queryparams = {
+            @ApiQueryParam(name = "email", description = "The email of user which logged user wants to make a relationship")
+    })
+    @ApiErrors(apierrors = {
+            @ApiError(code = "id_not_specified", description = "if email was not specified"),
+            @ApiError(code = "user_not_exist", description = "If user with specified email does not exist"),
+            @ApiError(code = "relation_already_exists", description = "If relation that logged user wants to create already exists")
+    })
     @RequestMapping(value = "/add", method = RequestMethod.POST)
     public @ResponseBody Relation addRelation(String email) throws IdNotSpecifiedException, UserException, RelationException {
         if(Strings.isNullOrEmpty(email)){
-            throw new IdNotSpecifiedException("id_not_specified");
+            throw new IdNotSpecifiedException("email_not_specified");
         }
         Optional<User> maybeUser = usersService.findByEmail(email);
         if(!maybeUser.isPresent()){
@@ -39,10 +50,19 @@ public class RelationController extends BaseController {
                 user.getEmail(),user.getFirstName(),user.getLastName()));
     }
 
+    @ApiMethod(description = "service to remove relation")
+    @ApiParams(queryparams = {
+            @ApiQueryParam(name = "email", description = "The email of user which logged user wants to discontinue relationship")
+    })
+    @ApiErrors(apierrors = {
+            @ApiError(code = "email_not_specified", description = "If email of user was not specified"),
+            @ApiError(code = "user_not_exist", description = "If user which email was specified does not exist"),
+            @ApiError(code = "relation_not_exist", description = "If relationship which is wanted to be discontinued does not exist")
+    })
     @RequestMapping(value = "/remove", method = RequestMethod.GET)
     public void removeRelation(String email) throws IdNotSpecifiedException, UserException, RelationException {
         if(Strings.isNullOrEmpty(email)){
-            throw new IdNotSpecifiedException("id_not_specified");
+            throw new IdNotSpecifiedException("email_not_specified");
         }
         Optional<User> maybeUser = usersService.findByEmail(email);
         if(!maybeUser.isPresent()){
@@ -54,11 +74,13 @@ public class RelationController extends BaseController {
                 user.getEmail(),user.getFirstName(),user.getLastName()));
     }
 
+    @ApiMethod(description = "service to get all emails of users that follows logged user")
     @RequestMapping(value = "/followers", method = RequestMethod.GET)
     public @ResponseBody List<String> getFollowers() throws UserException {
         return relationService.getAllFollowers();
     }
 
+    @ApiMethod(description = "service to get all email of users that are followed by logged user")
     @RequestMapping(value = "/followed", method = RequestMethod.GET)
     public @ResponseBody List<String> getFollowed() throws UserException {
         return relationService.getAllFollowedUsers();
