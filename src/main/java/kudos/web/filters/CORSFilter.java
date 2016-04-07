a2f948ba2ccd3ac36c542539cf4ee0fbbc06c44f
Vytautas Sugintas <vytautas.sugintas@gmail.com>
2016-04-07 17:07:19 +0300

    Updated CORSFilter

--- src/main/java/kudos/web/filters/CORSFilter.java
+++ src/main/java/kudos/web/filters/CORSFilter.java
@@ -3,6 +3,7 @@ package kudos.web.filters;
 import org.springframework.stereotype.Component;
 
 import javax.servlet.*;
+import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
@@ -11,11 +12,17 @@ public class CORSFilter implements Filter {
 
     public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain) throws IOException, ServletException {
         HttpServletResponse response = (HttpServletResponse) res;
-        response.setHeader("Access-Control-Allow-Origin", "*");
+        HttpServletRequest request = (HttpServletRequest) req;
+        response.setHeader("Access-Control-Allow-Origin", "http://localhost:63342");                             //TODO
+        response.setHeader("Access-Control-Allow-Credentials", "true");                             //TODO
         response.setHeader("Access-Control-Allow-Methods", "POST, GET, OPTIONS, DELETE");
         response.setHeader("Access-Control-Max-Age", "3600");
         response.setHeader("Access-Control-Allow-Headers", "x-requested-with");
-        chain.doFilter(req, res);
+        if ("OPTIONS".equalsIgnoreCase(request.getMethod())){
+            response.setStatus(HttpServletResponse.SC_OK);
+        }else {
+            chain.doFilter(req, res);
+        }
     }
 
     public void init(FilterConfig filterConfig) {
