ccb05770fa0c8ee32e02a29f046bdf1c667101c9
Vytautas Sugintas <vytautas.sugintas@gmail.com>
2016-05-05 11:58:45 +0300

    Added one more dev domain to cors filter

--- src/main/java/kudos/web/filters/CORSFilter.java
+++ src/main/java/kudos/web/filters/CORSFilter.java
@@ -13,8 +13,9 @@ public class CORSFilter implements Filter {
     public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain) throws IOException, ServletException {
         HttpServletResponse response = (HttpServletResponse) res;
         HttpServletRequest request = (HttpServletRequest) req;
-        response.setHeader("Access-Control-Allow-Origin", "http://localhost:63342");                             //TODO
-        response.setHeader("Access-Control-Allow-Credentials", "true");                             //TODO
+        response.setHeader("Access-Control-Allow-Origin", "http://localhost:63342");
+        response.setHeader("Access-Control-Allow-Origin", "http://localhost:3000");
+        response.setHeader("Access-Control-Allow-Credentials", "true");
         response.setHeader("Access-Control-Allow-Methods", "POST, GET, OPTIONS, DELETE");
         response.setHeader("Access-Control-Max-Age", "3600");
         response.setHeader("Access-Control-Allow-Headers", "x-requested-with");
