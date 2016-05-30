d847ac7d53561fb7cff6ecdd65d44be52204dc1f
Robertas <robertasvainickas@gmail.com>
2016-05-30 14:48:28 +0300

    Created team challenge model and transfer form

---
+++ src/main/java/kudos/model/TeamChallenge.java
@@ -0,0 +1,121 @@
+package kudos.model;
+
+import com.sun.org.apache.xpath.internal.operations.Bool;
+import org.jsondoc.core.annotation.ApiObject;
+import org.jsondoc.core.annotation.ApiObjectField;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.util.List;
+import java.util.Map;
+
+@ApiObject
+@Document
+public class TeamChallenge {
+
+    public enum Status {
+        CREATED, ACCEPTED, DECLINED, ACCOMPLISHED, FAILED, CANCELED
+    }
+
+    @Id
+    private String id;
+    @ApiObjectField
+    private String name;
+    @ApiObjectField
+    private Map<String, Boolean> firstTeam;
+    @ApiObjectField
+    private Map<String, Boolean> secondTeam;
+    @ApiObjectField
+    private String description;
+    @ApiObjectField
+    private String finishDate;
+    @ApiObjectField
+    private int amount;
+    @ApiObjectField
+    private Status firstTeamStatus;
+    @ApiObjectField
+    private Status secondTeamStatus;
+
+    public TeamChallenge(String name, Map<String, Boolean> firstTeam, Map<String, Boolean> secondTeam, String description, String finishDate, int amount, Status firstTeamStatus, Status secondTeamStatus) {
+        this.name = name;
+        this.firstTeam = firstTeam;
+        this.secondTeam = secondTeam;
+        this.description = description;
+        this.finishDate = finishDate;
+        this.amount = amount;
+        this.firstTeamStatus = firstTeamStatus;
+        this.secondTeamStatus = secondTeamStatus;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public String getFinishDate() {
+        return finishDate;
+    }
+
+    public int getAmount() {
+        return amount;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public void setFinishDate(String finishDate) {
+        this.finishDate = finishDate;
+    }
+
+    public void setAmount(int amount) {
+        this.amount = amount;
+    }
+
+    public Map<String, Boolean> getFirstTeam() {
+        return firstTeam;
+    }
+
+    public Map<String, Boolean> getSecondTeam() {
+        return secondTeam;
+    }
+
+    public void setFirstTeam(Map<String, Boolean> firstTeam) {
+        this.firstTeam = firstTeam;
+    }
+
+    public void setSecondTeam(Map<String, Boolean> secondTeam) {
+        this.secondTeam = secondTeam;
+    }
+
+    public Status getFirstTeamStatus() {
+        return firstTeamStatus;
+    }
+
+    public Status getSecondTeamStatus() {
+        return secondTeamStatus;
+    }
+
+    public void setFirstTeamStatus(Status firstTeamStatus) {
+        this.firstTeamStatus = firstTeamStatus;
+    }
+
+    public void setSecondTeamStatus(Status secondTeamStatus) {
+        this.secondTeamStatus = secondTeamStatus;
+    }
+}
---
+++ src/main/java/kudos/web/beans/form/TeamChallengeTransferForm.java
@@ -0,0 +1,145 @@
+package kudos.web.beans.form;
+
+import com.google.common.base.Strings;
+import org.joda.time.format.DateTimeFormat;
+import org.jsondoc.core.annotation.ApiObject;
+import org.jsondoc.core.annotation.ApiObjectField;
+import org.springframework.validation.Errors;
+import org.springframework.validation.Validator;
+
+import java.util.Map;
+
+@ApiObject
+public class TeamChallengeTransferForm {
+    @ApiObjectField
+    private String name;
+    @ApiObjectField
+    private Map<String, Boolean> firstTeam;
+    @ApiObjectField
+    private Map<String, Boolean> secondTeam;
+    @ApiObjectField
+    private String description;
+    @ApiObjectField
+    private String finishDate;
+    @ApiObjectField
+    private String amount;
+
+    public String getName() {
+        return name;
+    }
+
+    public Map<String, Boolean> getFirstTeam() {
+        return firstTeam;
+    }
+
+    public Map<String, Boolean> getSecondTeam() {
+        return secondTeam;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public String getFinishDate() {
+        return finishDate;
+    }
+
+    public String getAmount() {
+        return amount;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setFirstTeam(Map<String, Boolean> firstTeam) {
+        this.firstTeam = firstTeam;
+    }
+
+    public void setSecondTeam(Map<String, Boolean> secondTeam) {
+        this.secondTeam = secondTeam;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public void setFinishDate(String finishDate) {
+        this.finishDate = finishDate;
+    }
+
+    public void setAmount(String amount) {
+        this.amount = amount;
+    }
+
+    public static class ChallengeTransferFormValidator implements Validator {
+
+        private static final String EMAIL_PATTERN = "^[_A-Za-z0-9-\\+]+(\\.[_A-Za-z0-9-]+)*" +
+                "@[A-Za-z0-9-]+(\\.[A-Za-z0-9]+)*(\\.[A-Za-z]{2,})$";
+
+        @Override
+        public boolean supports(Class<?> clazz) {
+            return false;
+        }
+
+        @Override
+        public void validate(Object target, Errors errors) {
+            TeamChallengeTransferForm teamChallengeTransferForm = (TeamChallengeTransferForm) target;
+
+            for (Map.Entry<String, Boolean> participant : teamChallengeTransferForm.getFirstTeam().entrySet()) {
+                String participantEmail = participant.getKey();
+                if (Strings.isNullOrEmpty(participantEmail)) {
+                    errors.rejectValue("participant", "participant_email_not_specified");
+                } else if (!participantEmail.matches(EMAIL_PATTERN)) {
+                    errors.rejectValue("participant", "participant_email_incorrect");
+                }
+            }
+
+            for (Map.Entry<String, Boolean> participant : teamChallengeTransferForm.getSecondTeam().entrySet()) {
+                String participantEmail = participant.getKey();
+                if (Strings.isNullOrEmpty(participantEmail)) {
+                    errors.rejectValue("participant", "participant_email_not_specified");
+                } else if (!participantEmail.matches(EMAIL_PATTERN)) {
+                    errors.rejectValue("participant", "participant_email_incorrect");
+                }
+            }
+
+            String amountInString = teamChallengeTransferForm.getAmount();
+            String challengeName = teamChallengeTransferForm.getName();
+            String estimatedDate = teamChallengeTransferForm.getFinishDate();
+
+            if (Strings.isNullOrEmpty(amountInString)) {
+                errors.rejectValue("amount", "amount_not_specified");
+            }
+
+            try {
+                int amount = Integer.parseInt(amountInString);
+                if (amount <= 0) {
+                    errors.rejectValue("amount", "amount_negative_or_zero");
+                }
+            } catch (NumberFormatException e) {
+                errors.rejectValue("amount", "amount_not_digit");
+            }
+
+            if (Strings.isNullOrEmpty(challengeName)) {
+                errors.rejectValue("name", "name_not_specified");
+            }
+
+            if (!Strings.isNullOrEmpty(estimatedDate)) {
+                if (!isEnteredDateValid(estimatedDate)) {
+                    errors.rejectValue("finishDate", "finishDate_incorrect");
+                }
+            }
+        }
+
+        public boolean isEnteredDateValid(String text) {
+            try {
+                DateTimeFormat.forPattern("yyyy-MM-dd HH:mm:ss,SSS").parseLocalDate(text);
+                return true;
+            } catch (IllegalArgumentException p) {
+                return false;
+            }
+
+        }
+    }
+}
