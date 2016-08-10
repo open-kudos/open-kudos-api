e93ce0c1b0a62f6ae2b43f65f3ddb5e26559cf3d
Vytautas Sugintas <vytautas.sugintas@gmail.com>
2016-08-10 16:46:53 +0300

    not removing canceled challenges, instead changing status to CANCELED

--- src/main/java/kudos/repositories/ActionRepository.java
+++ src/main/java/kudos/repositories/ActionRepository.java
@@ -1,11 +1,13 @@
 package kudos.repositories;
 
 import kudos.model.Action;
+import kudos.model.Challenge;
 import kudos.model.Relation;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
 public interface ActionRepository extends MongoRepository<Action, String> {
 
     long deleteByRelation(Relation relation);
+    long deleteByChallenge(Challenge challenge);
 
 }
