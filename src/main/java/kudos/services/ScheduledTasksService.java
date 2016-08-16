dd52b4da689741fc9b097c5511f5c61932fdc508
Vytautas Sugintas <vytautas.sugintas@gmail.com>
2016-08-16 15:28:15 +0300

    Added scheduled task to look for expired challenges every 24h

--- src/main/java/kudos/services/ScheduledTasksService.java
+++ src/main/java/kudos/services/ScheduledTasksService.java
@@ -1,39 +1,32 @@
 package kudos.services;
 
+import kudos.model.Challenge;
+import kudos.model.ChallengeStatus;
+import kudos.repositories.ChallengeRepository;
+import org.joda.time.LocalDateTime;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
+
+import java.util.List;
 
 @Component
 public class ScheduledTasksService {
 
+    @Autowired
+    ChallengeService challengeService;
 
-//    @Autowired
-//    ChallengeService challengeService;
-//
-//    @Autowired
-//    @Qualifier(value = "DBTimeFormatter")
-//    DateTimeFormatter dateTimeFormatter;
-//
-//    @Scheduled(fixedRate = 1000 * 15)
-//    public void markTasksAsExpired() {
-//        List<Challenge> challengesToCheck = new ArrayList<>();
-//        challengesToCheck.addAll(challengeService.getAllAcceptedChallenges());
-//        challengesToCheck.addAll(challengeService.getAllCreatedChallenges());
-//
-//        LOG.info("challengesToCheck challenges amount is: " + challengesToCheck.size());
-//
-//        challengesToCheck.stream()
-//                .filter(c -> !c.getStatus().equals(ChallengeStatus.EXPIRED))
-//                .filter(c -> !(c.getExpirationDate() == null))
-//                .filter(c -> dateTimeFormatter.parseLocalDateTime(c.getExpirationDate()).isBefore(LocalDateTime.now()))
-//                .forEach((challenge) -> {
-//                    try {
-//                        challengeService.expire(challenge);
-//                    } catch (BusinessException e) {
-//                        e.printStackTrace();
-//                    } catch (UserException e) {
-//                        e.printStackTrace();
-//                    }
-//                });
-//    }
+    @Autowired
+    ChallengeRepository challengeRepository;
+
+    @Scheduled(fixedRate = 1000 * 60 * 24)
+    public void markTasksAsExpired() {
+        List<Challenge> challengeList = challengeRepository.findAllChallengesByStatus(ChallengeStatus.CREATED);
+        challengeList.addAll(challengeRepository.findAllChallengesByStatus(ChallengeStatus.ACCEPTED));
+        challengeList.stream().filter(challenge -> challenge.getExpirationDate() != null && LocalDateTime.parse(challenge.getExpirationDate()).isBefore(LocalDateTime.now())).forEach(challenge -> {
+            challengeService.markChallengeAsExpired(challenge);
+        });
+
+    }
 }
 
